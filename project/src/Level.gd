3219ca30a14b6a65e71d43f1bd05245c531f934a
curtis clemmons <curtisclemmons2000@icloud.com>
2020-10-04 16:18:12 -0400

    Add reset button and return to title button

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,2 +1,9 @@
 extends Node2D
 
+func _process(_delta):
+	if Input.is_action_just_released("reset"):
+		var _ignored = get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("return_to_title"):
+		var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
+		
