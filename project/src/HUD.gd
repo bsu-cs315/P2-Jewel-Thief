255164fcb4bba7df4aaae7f0a005c239093d3a0f
curtis clemmons <curtisclemmons2000@icloud.com>
2020-10-04 17:08:17 -0400

    Change HUD layout

--- project/src/HUD.gd
+++ project/src/HUD.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 
 func update_jewels_remaining(jewels_remaining):
-	$JewelsRemaining.text = "Jewels Remaining: " + str(jewels_remaining)
+	var jewels_remaining_label = get_child(0).get_child(0)
+	jewels_remaining_label.text = "Jewels Remaining: " + str(jewels_remaining)
 
 
 func update_jewels_collected(jewel_collected):
-	$JewelsCollected.text = "Jewels Collected: " + str(jewel_collected)
+	var jewels_collected_label = get_child(0).get_child(1)
+	jewels_collected_label.text = "Jewels Collected: " + str(jewel_collected)
