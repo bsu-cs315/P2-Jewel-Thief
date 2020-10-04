4db0ec33fa05c1930b04f30399b0c1d313288cc6
curtis clemmons <curtisclemmons2000@icloud.com>
2020-10-04 16:33:10 -0400

    Add end screen

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -6,4 +6,10 @@ func _process(_delta):
 		
 	if Input.is_action_just_pressed("return_to_title"):
 		var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
-		
+	 
+	check_for_gameover()
+
+
+func check_for_gameover():
+	if HUDManager.jewels_collected == 3:
+		var _ignored = get_tree().change_scene("res://src/EndScreen.tscn")
--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -1,4 +1,7 @@
 extends CanvasLayer
 
+
 func _on_Button_pressed():
 	var _ignored = get_tree().change_scene("res://src/Level.tscn")
+	HUDManager.jewels_collected = 0
+	HUDManager.jewels_remaining = 3
