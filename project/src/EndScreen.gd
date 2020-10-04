f6bce2b42bd8ac8c582a8ca0b4069fa68775f560
curtis clemmons <curtisclemmons2000@icloud.com>
2020-10-04 16:53:25 -0400

    Update endscreen

---
+++ project/src/EndScreen.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+func _process(_delta):
+	if Input.is_action_just_pressed("return_to_title"):
+		var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
+	
+	
+
+func _on_PlayAgain_pressed():
+	HUDManager.jewels_collected = 0
+	HUDManager.jewels_remaining = 3
+	var _ignored = get_tree().change_scene("res://src/Level.tscn")
+
+
+func _on_ReturnToTitle_pressed():
+	var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
